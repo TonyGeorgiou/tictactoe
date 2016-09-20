91854309623f46bb407ab31e47a06513502f6905
Andonis <andonisgeorgiou@gmail.comgit config --global user.email andonisgeorgiou@gmail.com>
2016-09-20 10:02:37 +1000

    initial commit

---
+++ js/tictactoe.js
@@ -0,0 +1,25 @@
+var board = [A1, A2, A3, B1, B2, B3, C3, C2, C3];
+
+//Use minmax values ???????
+
+//Code for no moving to position already played.
+
+//Random decision for who plays first.
+
+//If Carol first, display her move.
+
+//Check for win. If yes, display win.
+
+//Check for draw. If yes, display draw.
+
+//Switch to other player.
+
+//If Clown first, display his move.
+
+//Check for win. If yes, display win.
+
+//Check for draw. If yes, display draw.
+
+//Switch to other player.
+
+//Add score to game counter.
